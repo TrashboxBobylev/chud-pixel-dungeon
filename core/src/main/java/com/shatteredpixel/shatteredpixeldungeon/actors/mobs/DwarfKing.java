03e56e77cc98700907728e480203ea67d237bf1d
Trashbox Bobylev <trashbox.bobylev@gmail.com>
2026-07-28 03:03:27 +0700

    v2: nerfed dwarf king's health from 900 to 630

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/DwarfKing.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/DwarfKing.java
@@ -86,7 +86,7 @@ public class DwarfKing extends Mob {
 	{
 		spriteClass = KingSprite.class;
 
-		HP = HT = Dungeon.isChallenged(Challenges.STRONGER_BOSSES) ? 900 : 300;
+		HP = HT = Dungeon.isChallenged(Challenges.STRONGER_BOSSES) ? 630 : 300;
 		EXP = 40;
 		defenseSkill = 22;
 
